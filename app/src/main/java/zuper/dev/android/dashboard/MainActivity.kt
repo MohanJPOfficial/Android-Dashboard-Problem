d7f5f855e38395ed175915a465b01213ac8e6545
Mohan <jpmohan007@gmail.com>
2024-02-22 10:26:36 +0530

    initial commit

---
+++ app/src/main/java/zuper/dev/android/dashboard/MainActivity.kt
@@ -0,0 +1,60 @@
+package zuper.dev.android.dashboard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import zuper.dev.android.dashboard.data.DataRepository
+import zuper.dev.android.dashboard.data.remote.ApiDataSource
+import zuper.dev.android.dashboard.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val rep  = DataRepository(ApiDataSource())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+
+                    lifecycleScope.launch {
+                        rep.observeJobs().collectLatest {
+                            println(it)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppTheme {
+        Greeting("Android")
+    }
+}
