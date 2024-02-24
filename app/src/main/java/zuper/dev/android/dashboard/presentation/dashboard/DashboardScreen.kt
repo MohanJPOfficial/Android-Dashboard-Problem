1f315f5c5ae685bc262b8e53eb79d5932c28571a
Mohan <jpmohan007@gmail.com>
2024-02-24 16:28:49 +0530

    added vertical scroll on landscape

--- app/src/main/java/zuper/dev/android/dashboard/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/zuper/dev/android/dashboard/presentation/dashboard/DashboardScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,6 +35,7 @@ fun DashboardScreen(
     Surface(
         color = MaterialTheme.colorScheme.onSecondary,
         modifier = modifier
+            .verticalScroll(rememberScrollState())
             .fillMaxSize()
     ) {
         Column {
